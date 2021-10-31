f3fe074c6a4d6b6ec98ee1fc583cfeb003757628
Easin Arafat <arafatmrx@gmail.com>
2021-10-31 10:29:46 +0600

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = 4000;
+
+//uri
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//client
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
