ca3714c901ff9017aa216f5ad8125d973c55c8f7
Easin Arafat <arafatmrx@gmail.com>
2021-11-01 23:25:17 +0600

    index

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //uri
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 //client
 
@@ -16,7 +22,34 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-app.get("/", (req, res) => {
+//async function
+
+async function run() {
+  try {
+    client.connect();
+
+    const database = client.db("travelAgency");
+    const servicesCollection = database.collection("services");
+
+    //post api
+
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+
+      console.log("hit the post api", service);
+
+      const result = await servicesCollection.insertOne(service);
+      res.send("post hitted");
+    });
+
+    console.log("database connected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/services", (req, res) => {
   res.send("Hello World!");
 });
 
