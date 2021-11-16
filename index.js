9e70fd10ec1a2441613933d84139a9bffb35db40
Easin Arafat <arafatmrx@gmail.com>
2021-11-17 00:30:27 +0600

    db established

--- index.js
+++ index.js
@@ -1,58 +1,92 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-
-const app = express();
-const port = process.env.PORT || 4000;
+require("dotenv").config();
 
 //uri
 
+const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-console.log(uri);
-
-//client
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 //async function
-
 async function run() {
   try {
-    client.connect();
-
-    const database = client.db("travelAgency");
+    await client.connect();
+    const database = client.db("Travel_Agency");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
     const servicesCollection = database.collection("services");
 
-    //post api
+    //get api
 
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const cursor = ordersCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+    //service api
     app.post("/services", async (req, res) => {
       const service = req.body;
-
-      console.log("hit the post api", service);
-
       const result = await servicesCollection.insertOne(service);
-      res.send("post hitted");
+      res.json(result);
+    });
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.json(services);
     });
 
-    console.log("database connected");
+    //post api for create orders
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+
+      console.log(result);
+      //sent to front-end
+      res.json(result);
+    });
+    //post api for create users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-app.get("/services", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello The Travel !");
 });
 
 app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
+  console.log(`listening at ${port}`);
 });
